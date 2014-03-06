a4568dd6bc1953125371a3147ed1e74ef51734d3
ase34 <asehrm34@gmail.com>
2014-03-06 21:22:48 +0100

    Formatted license headers

--- src/main/java/de/ase34/flyingblocksapi/util/EntityRegistrator.java
+++ src/main/java/de/ase34/flyingblocksapi/util/EntityRegistrator.java
@@ -1,23 +1,18 @@
 /**
- * flyingblocksapi
- * Copyright (C) 2014 ase34 and contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * flyingblocksapi Copyright (C) 2014 ase34 and contributors
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 /*
- * This code is pulled from Jogy34 
- * (https://forums.bukkit.org/threads/tutorial-1-7-creating-a-custom-entity.212849/)
+ * This code is pulled from Jogy34 (https://forums.bukkit.org/threads/tutorial-1-7-creating-a-custom-entity.212849/)
  */
 package de.ase34.flyingblocksapi.util;
 
@@ -26,17 +21,20 @@ import java.util.Map;
 
 public class EntityRegistrator {
 
-    protected static Field mapStringToClassField, mapClassToStringField, mapClassToIdField, mapStringToIdField;
+    protected static Field mapStringToClassField, mapClassToStringField, mapClassToIdField,
+            mapStringToIdField;
 
-    static
-    {
-        try
-        {
-            mapStringToClassField = net.minecraft.server.v1_7_R1.EntityTypes.class.getDeclaredField("c");
-            mapClassToStringField = net.minecraft.server.v1_7_R1.EntityTypes.class.getDeclaredField("d");
+    static {
+        try {
+            mapStringToClassField = net.minecraft.server.v1_7_R1.EntityTypes.class
+                    .getDeclaredField("c");
+            mapClassToStringField = net.minecraft.server.v1_7_R1.EntityTypes.class
+                    .getDeclaredField("d");
             // mapIdtoClassField = net.minecraft.server.v1_7_R1.EntityTypes.class.getDeclaredField("e");
-            mapClassToIdField = net.minecraft.server.v1_7_R1.EntityTypes.class.getDeclaredField("f");
-            mapStringToIdField = net.minecraft.server.v1_7_R1.EntityTypes.class.getDeclaredField("g");
+            mapClassToIdField = net.minecraft.server.v1_7_R1.EntityTypes.class
+                    .getDeclaredField("f");
+            mapStringToIdField = net.minecraft.server.v1_7_R1.EntityTypes.class
+                    .getDeclaredField("g");
 
             mapStringToClassField.setAccessible(true);
             mapClassToStringField.setAccessible(true);
@@ -49,17 +47,12 @@ public class EntityRegistrator {
     }
 
     @SuppressWarnings({ "rawtypes", "unchecked" })
-    public static void registerCustomEntity(Class entityClass, String name, int id)
-    {
-        if (mapStringToClassField == null || mapStringToIdField == null || mapClassToStringField == null
-                || mapClassToIdField == null)
-        {
+    public static void registerCustomEntity(Class entityClass, String name, int id) {
+        if (mapStringToClassField == null || mapStringToIdField == null
+                || mapClassToStringField == null || mapClassToIdField == null) {
             return;
-        }
-        else
-        {
-            try
-            {
+        } else {
+            try {
                 Map mapStringToClass = (Map) mapStringToClassField.get(null);
                 Map mapStringToId = (Map) mapStringToIdField.get(null);
                 Map mapClasstoString = (Map) mapClassToStringField.get(null);
@@ -74,8 +67,7 @@ public class EntityRegistrator {
                 mapStringToIdField.set(null, mapStringToId);
                 mapClassToStringField.set(null, mapClasstoString);
                 mapClassToIdField.set(null, mapClassToId);
-            } catch (Exception e)
-            {
+            } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }
