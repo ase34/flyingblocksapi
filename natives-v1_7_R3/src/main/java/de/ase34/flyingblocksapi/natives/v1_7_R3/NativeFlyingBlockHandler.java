eac1c08a9eb100ae446fd6ea66e1846aac150e63
ase34 <asehrm34@gmail.com>
2014-04-25 10:11:23 +0200

    Added natives for v1_7_R3

---
+++ natives-v1_7_R3/src/main/java/de/ase34/flyingblocksapi/natives/v1_7_R3/NativeFlyingBlockHandler.java
@@ -0,0 +1,68 @@
+package de.ase34.flyingblocksapi.natives.v1_7_R3;
+
+import net.minecraft.server.v1_7_R3.EntityWitherSkull;
+import net.minecraft.server.v1_7_R3.World;
+
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_7_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_7_R3.entity.CraftWitherSkull;
+import org.bukkit.entity.WitherSkull;
+
+import de.ase34.flyingblocksapi.FlyingBlock;
+import de.ase34.flyingblocksapi.natives.v1_7_R3.util.SpawnHelper;
+
+public class NativeFlyingBlockHandler extends de.ase34.flyingblocksapi.natives.api.NativeFlyingBlockHandler {
+
+    private EntityWitherSkull entity;
+    
+    public NativeFlyingBlockHandler(FlyingBlock flyingBlock) {
+        super(flyingBlock);
+    }
+
+    @Override
+    public void spawnFlyingBlock(Location location) {
+        if (entity != null && entity.isAlive()) {
+            flyingBlock.remove();
+        }
+
+        entity = SpawnHelper.spawn(flyingBlock, location);
+    }
+
+    @Override
+    public void removeEntites() {
+        if (entity == null) {
+            return;
+        }
+
+        entity.passenger.passenger.die();
+        entity.passenger.die();
+        entity.die();
+        entity = null;
+    }
+
+    @Override
+    public void setFlyingBlockLocation(Location location) {
+        if (entity == null || !entity.isAlive()) {
+            throw new UnsupportedOperationException("The entity was not spawned yet!");
+        }
+
+        World newWorld = ((CraftWorld) location.getWorld()).getHandle();
+        if (newWorld != entity.world) {
+            flyingBlock.remove();
+            flyingBlock.spawn(location);
+        } else {
+            entity.setLocation(location.getX(), location.getY(), location.getZ(),
+                    location.getYaw(), location.getPitch());
+        }
+    }
+
+    @Override
+    public WitherSkull getBukkitEntity() {
+        return getCraftBukkitEntity();
+    }
+
+    public CraftWitherSkull getCraftBukkitEntity() {
+        return (CraftWitherSkull) entity.getBukkitEntity();
+    }
+
+}
---
+++ natives-v1_7_R3/src/main/java/de/ase34/flyingblocksapi/natives/v1_7_R3/util/SpawnHelper.java
@@ -0,0 +1,60 @@
+/**
+ * flyingblocksapi Copyright (C) 2014 ase34 and contributors
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package de.ase34.flyingblocksapi.natives.v1_7_R3.util;
+
+import net.minecraft.server.v1_7_R3.EntityFallingBlock;
+import net.minecraft.server.v1_7_R3.EntityHorse;
+import net.minecraft.server.v1_7_R3.EntityWitherSkull;
+import net.minecraft.server.v1_7_R3.World;
+
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_7_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_7_R3.util.CraftMagicNumbers;
+
+import de.ase34.flyingblocksapi.FlyingBlock;
+import de.ase34.flyingblocksapi.natives.v1_7_R3.entities.CustomFallingBlock;
+import de.ase34.flyingblocksapi.natives.v1_7_R3.entities.CustomHorse;
+import de.ase34.flyingblocksapi.natives.v1_7_R3.entities.CustomWitherSkull;
+
+public class SpawnHelper {
+
+    public static EntityWitherSkull spawn(FlyingBlock flyingBlock, Location location) {
+        World world = ((CraftWorld) location.getWorld()).getHandle();
+
+        double x = location.getX();
+        double y = location.getY();
+        double z = location.getZ();
+
+        EntityWitherSkull skull = new CustomWitherSkull(flyingBlock, world);
+        skull.setPosition(x, y + flyingBlock.getHeightOffset(), z);
+        world.addEntity(skull);
+
+        EntityHorse horse = new CustomHorse(world);
+        horse.setAge(flyingBlock.getHorseAge());
+        horse.setPosition(x, y + flyingBlock.getHeightOffset(), z);
+        world.addEntity(horse);
+
+        EntityFallingBlock block = new CustomFallingBlock(horse, world);
+        block.id = CraftMagicNumbers.getBlock(flyingBlock.getMaterial());
+        block.data = flyingBlock.getMaterialData();
+        block.setPosition(x, y + flyingBlock.getHeightOffset(), z);
+        world.addEntity(block);
+
+        block.setPassengerOf(horse);
+        horse.setPassengerOf(skull);
+
+        return skull;
+    }
+
+}
