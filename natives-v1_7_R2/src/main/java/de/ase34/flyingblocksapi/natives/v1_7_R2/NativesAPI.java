f31a4e9278906ed1b28b20ce6b8fe4453547d76e
ase34 <asehrm34@gmail.com>
2014-04-25 20:52:16 +0000

    Added natives for v1_7_R2

---
+++ natives-v1_7_R2/src/main/java/de/ase34/flyingblocksapi/natives/v1_7_R2/NativesAPI.java
@@ -0,0 +1,53 @@
+package de.ase34.flyingblocksapi.natives.v1_7_R2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_7_R2.CraftWorld;
+import org.bukkit.entity.Entity;
+
+import de.ase34.flyingblocksapi.FlyingBlock;
+import de.ase34.flyingblocksapi.natives.v1_7_R2.entities.CustomFallingBlock;
+import de.ase34.flyingblocksapi.natives.v1_7_R2.entities.CustomHorse;
+import de.ase34.flyingblocksapi.natives.v1_7_R2.entities.CustomWitherSkull;
+import de.ase34.flyingblocksapi.natives.v1_7_R2.util.EntityRegistrator;
+
+public class NativesAPI extends de.ase34.flyingblocksapi.natives.api.NativesAPI {
+
+    @Override
+    public List<Entity> removeFlyingBlocks(World world) {
+        ArrayList<Entity> entities = new ArrayList<Entity>();
+        net.minecraft.server.v1_7_R2.World nmsworld = ((CraftWorld) world).getHandle();
+
+        for (Object entityObject : nmsworld.entityList) {
+            net.minecraft.server.v1_7_R2.Entity entity = (net.minecraft.server.v1_7_R2.Entity) entityObject;
+
+            if (entity instanceof CustomFallingBlock) {
+                entity.die();
+                entities.add(entity.getBukkitEntity());
+            } else if (entity instanceof CustomHorse) {
+                entity.die();
+                entities.add(entity.getBukkitEntity());
+            } else if (entity instanceof CustomWitherSkull) {
+                entity.die();
+                entities.add(entity.getBukkitEntity());
+            }
+        }
+
+        return entities;
+    }
+
+    @Override
+    public void initialize() {
+        EntityRegistrator.registerCustomEntity(CustomFallingBlock.class, "fba.fallingblock", 21);
+        EntityRegistrator.registerCustomEntity(CustomHorse.class, "fba.horse", 100);
+        EntityRegistrator.registerCustomEntity(CustomWitherSkull.class, "fba.witherskull", 19);
+    }
+
+    @Override
+    public de.ase34.flyingblocksapi.natives.api.NativeFlyingBlockHandler createFlyingBlockHandler(FlyingBlock flyingBlock) {
+        return new NativeFlyingBlockHandler(flyingBlock);
+    }
+
+}
---
+++ natives-v1_7_R2/src/main/java/de/ase34/flyingblocksapi/natives/v1_7_R2/entities/CustomWitherSkull.java
@@ -0,0 +1,60 @@
+/**
+ * flyingblocksapi Copyright (C) 2014 ase34 and contributors
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package de.ase34.flyingblocksapi.natives.v1_7_R2.entities;
+
+import de.ase34.flyingblocksapi.FlyingBlock;
+import net.minecraft.server.v1_7_R2.EntityTracker;
+import net.minecraft.server.v1_7_R2.EntityWitherSkull;
+import net.minecraft.server.v1_7_R2.World;
+import net.minecraft.server.v1_7_R2.WorldServer;
+
+public class CustomWitherSkull extends EntityWitherSkull {
+
+    private final FlyingBlock flyingBlock;
+
+    private boolean initial = true;
+
+    public CustomWitherSkull(FlyingBlock flyingBlock, World world) {
+        super(world);
+        this.flyingBlock = flyingBlock;
+    }
+
+    @Override
+    public void h() {
+        // tick entity
+        flyingBlock.onTick();
+
+        // direction is acceleration
+        this.motX += this.dirX;
+        this.motY += this.dirY;
+        this.motZ += this.dirZ;
+
+        // motion is speed
+        this.locX += this.motX;
+        this.locY += this.motY;
+        this.locZ += this.motZ;
+
+        this.setPosition(this.locX, this.locY, this.locZ);
+
+        if (initial) {
+            // set tracker
+            EntityTracker tracker = ((WorldServer) this.world).tracker;
+            tracker.untrackEntity(this);
+            tracker.addEntity(this, 64, flyingBlock.getTrackerUpdateInterval());
+
+            initial = true;
+        }
+    }
+
+}
